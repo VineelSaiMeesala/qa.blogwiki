ce98eafe089fb721be0082ed86b4cc053c5956bb
Meesala Vineel Sai <85602650+VineelSaiMeesala@users.noreply.github.com>
2024-03-01 09:51:46 +0530

    QA config

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 const configurations ={
-    Environment:"DEV",
-    Version: 'V49.MJ-06.01.24-Dev',
+    Environment:"QA",
+    Version: 'V89.MJ-06.01.24-Dev',
     testkey:'2R3Gu1DsdrjUdk5KyaTO'
 }
 function config (passKey){
